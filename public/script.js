3c726e176253a50b88ed9aba2d402277ca632b59
rkylerd <rkylerd06@gmail.com>
2019-06-03 22:10:16 -0600

    Good Reads 3.0

--- public/script.js
+++ public/script.js
@@ -1,25 +1,34 @@
 let app = new Vue({
+    el: '#app',
     data: {
-        query: 'the+lord+of+the+rings',
+        query: '',
         bookName: '',
         counter: 0,
         loading: true,
         current: {},
-        results: {
+        results: [{
             title: '',
             author: '',
             isbn: [],
-        },
+        }],
+        pastFavorites: [],
         numFound: '',
         thumbnails: [],
         isbn: '',
+        save: false,
+        tempFavorites: [],
     },
     created() {
-        this.goodReads();
+        // this.goodReads();
     },
     methods: {
+        saveChanges() {
+            this.pastFavorites.push(...this.tempFavorites);
+        },
         queryBook() {
-            console.log("no good: ", query);
+            this.results = [];
+
+            
             if (this.bookName === '')
                 return;
 
@@ -27,105 +36,136 @@ let app = new Vue({
             let query = "";
             for (var i = 0; i < searchTerms.length; i++) {
 
+                query += searchTerms[i].toLowerCase();
+
                 if (i !== searchTerms.length-1) {
-                    query += searchTerms[i].toLowerCase() + "+";
+                    query += "+";
                 }
             }
-            console.log("the search Term: ", query);
+            
             this.query = query;
+            this.goodReads();
         },
         async goodReads() {
             try {
                 this.loading = true;
-                const response = await axios.get("http://openlibrary.org/search.json?q=" + this.query);
-                this.results = response.data.docs;
-                // console.log("Response: ", response);
-
-                // console.log("results", this.results);
+                const response = await axios.get("https://openlibrary.org/search.json?q=" + this.query);
+                let queryResults = response.data.docs;
                 
                 this.numFound = response.data.numFound;
+                // console.log("Response num 1: ", queryResults);
                 
-
-                for (var i = 0; i < this.results.length; i++) {
+                let resultsCounter = 0;
+                for (var i = 0; i < queryResults.length; i++) {
                     
-                    this.counter++;
-                    if (this.results[i].isbn) {
+                    if (queryResults[i].isbn && queryResults[i].title && queryResults[i].publish_date && queryResults[i].author_name) {
                         
-                        // for (var y = 0; y < this.results[i].isbn.length; y++) {
-                            // console.log("i", i + ". " + this.results[i].isbn[y]);
-                            // await this.getThumbnail(this.results[i].isbn[y]);
                             try {
-                                const response2 = await axios.get("https://openlibrary.org/api/books?bibkeys=ISBN:" + this.results[i].isbn[0] + "&jscmd=viewapi&format=json");
-                                const response3 = await axios.get("https://openlibrary.org/api/books?bibkeys=ISBN:" + this.results[i].isbn[0] + "&jscmd=data&format=json");
-                                var keys = Object.keys(response3.data);
-                                
-                                var counter = 0;
-                                for (var name in response2.data ) {
-                                    counter++;
+                                const response2 = await axios.get("https://openlibrary.org/api/books?bibkeys=ISBN:" + queryResults[i].isbn[0] + "&jscmd=viewapi&format=json");
 
-                                    if (response2.data.hasOwnProperty(name))
-                                    {
+                                for (var name in response2.data ) {
+
+                                    if (response2.data.hasOwnProperty(name)) {
                                         let thumb = response2.data[name].thumbnail_url;
-                                        // console.log("author", response2.data[name].author);
+
+                                        // title, author, publish date, cover image,
                                         if (thumb) {
-                                            console.log("Thumb", thumb);
-                                            this.thumbnails.push(thumb);
+                                            this.addBook(thumb, queryResults[i], resultsCounter++);
                                         }
                                         
                                     }
                                     
                                 }
-                                
-                                
-                                
-                                // console.log(this.counter+ "urls: ", response2.data);
+                                                
                             } catch (err) {
                                 console.log(err);
-                
                             }
-                        // }
-                    }
+                    } 
                     
                 }
 
-                for (j = 0; j < this.thumbnails.length; j++) {
-                    console.log("thumbnails" + j, this.thumbnails[j]);
-                }
+                this.numFound = resultsCounter;
+
                 this.loading = false;
-                console.log(this.loading);
             } catch (err) {
                 console.log(err);
-
             }
             
         },
-        async getThumbnail() {
-            // try {
-            //     const response2 = await axios.get("https://openlibrary.org/api/books?bibkeys=ISBN:" + this.isbn + "&jscmd=details&format=json");
-                
-            //         // console.log("Should be a bunch of images: ", response.data);
-                
-            //     this.thumbnails.push(response2.data.thumbnail_url);
-                   
-            // } catch (err) {
-            //     console.log(err);
+        addBook(bookCover, bookInfo, index) {
+            if (!(index in this.results)) {
+                let pubDate = null;
 
-            // }
-            
-        }
+                if (bookInfo.author_name.length > 1) {
+                    var authString = "";
+                    for (var i = 0; i < bookInfo.author_name.length; i++) {
+                        if (i == bookInfo.author_name.length - 2) {
+                            authString += bookInfo.author_name[i] + " & " + bookInfo.author_name[++i];
+                            break;
+                        }
+                        authString += bookInfo.author_name[i] + ", ";
+                    }
+                    bookInfo.author_name = authString;
+                } else {
+                    bookInfo.author_name = bookInfo.author_name[0];
+                }
+
+                Vue.set(this.results, index, {title: bookInfo.title, author: bookInfo.author_name, publishDate: bookInfo.publish_date[0], bookCover: bookCover, favorited: false});
+            }
+        },
     },
     computed: {
+            favorites() {
+               
 
+                
+              return this.pastFavorites.filter(book => {
+                
+                return book.favorited;
+              });
+            },
     },
     watch: {
-        // query(value, oldvalue) {
-        //     if (oldvalue === '') {
-                
-        //       } else {
-        //         this.queryBook(value);
-        //         this.goodReads();
-        //       }
-        // },
-    }
+        favorites (newVal, oldVal) {
+            this.tempFavorites = newVal;
+
+            if (newVal.length == 0 &&oldVal.length ==0 && this.save) {
+                newVal = this.tempFavorites;
+                this.save = false;
+            }
+
+            console.log("oldValue: ", oldVal);
+            console.log("new value: ", newVal);
+
+            
+          },
+          results: {
+            handler: function (newResults, oldResults) {
+                console.log("oldResultsue: ", newResults);
+                console.log("new Resulst: ", oldResults);
+                for (var i = 0; i < newResults.length; i++) {
+                    var found = false;
+                    // for (var x = 0; x < oldResults.length; x++) {
+                    //     if (newResults[i].title != oldResults[x].title || newResults[i].author != newResults[x].author || newResults[i].bookCover != oldResults[x].bookCover || newResults[i].publishDate != oldResults[i].publishDate) {
+                            
+                            if (newResults[i].favorited && !this.pastFavorites.includes(newResults[i])) {
+                                
+                                console.log(newResults[i]);
+                                this.pastFavorites.push(newResults[i]);
+                            }
+                            
+                    //     }
+                    // }
+                    // if (oldResults.length == 0) {
+                    //     console.log(newResults[i]);
+                    //     this.pastFavorites.push(newResults[i]);
+                    // }
+                }
+            },
+            deep: true
+            },
+        }
+          
+        //   watch the results and cycle through to find the one that's new, then add to the pastFavoites which is what's displayed on the screen
 
 });
